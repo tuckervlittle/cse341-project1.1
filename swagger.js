b7f3c81ae5126a824f7a09ee70137aa21011b0b1
tuckervlittle <tucker.little95@gmail.com>
2025-11-08 14:39:45 -1000

    schemes 3

--- swagger.js
+++ swagger.js
@@ -5,12 +5,11 @@ const doc = {
     title: 'Contacts API',
     description: 'A simple API to manage contacts',
   },
-  host: process.env.HOST,
-  schemes: [process.env.SWAGGER_SCHEME],
+  host: process.env.SWAGGER_HOST || 'localhost:3000',
+  schemes: [(process.env.SWAGGER_SCHEME || 'http')],
 };
 
 const outputFile = './swagger.json';
 const endpointsFile = ['./routes/index.js'];
 
 swaggerAutogen(outputFile, endpointsFile, doc);
-console.log(outputFile);
